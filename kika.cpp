5541bd7ff62ff658a293f1dae78367c75b50bd45
cympfh <cympfh@yuugiri.(none)>
2013-04-25 11:56:04 +0900

    決め手はカニミソ 病みつき夢見そう

--- kika.cpp
+++ kika.cpp
@@ -2,10 +2,6 @@ float cross (float x1, float y1, float x2, float y2) {
 	return x1 * y2 - x2 * y1;
 }
 
-float dot (float x1, float y1, float x2, float y2) {
-	return x1 * x2 + y1 * y2;
-}
-
 int ccw (float x1, float y1, float x2, float y2, float x0, float y0) {
 	return cross(x1-x0, y1-y0, x2-x0, y2-y0) > 0 ? 1 : -1;
 }
